483db35423fc82c8b05b02634ad9cee56b8e3306
enbandari <305779913@qq.com>
2014-10-01 14:10:19 +0800

    整理NotifyIcon的代码，并添加窗口控制事件

--- GameDaemon/MainWindow.xaml.cs
+++ GameDaemon/MainWindow.xaml.cs
@@ -23,55 +23,88 @@ namespace GameDaemon
     /// </summary>
     public partial class MainWindow : Window
     {
-        private System.Windows.Forms.NotifyIcon notifyIcon;
-        private System.Windows.Forms.ContextMenu contextMenu1;
-        private System.Windows.Forms.MenuItem menuItem1;
+        private NotifyIcon notifyIcon;
+        private System.Windows.Forms.ContextMenu notifyMenu;
+        private System.Windows.Forms.MenuItem[] notifyMenuItems;
         //private System.ComponentModel.IContainer components;
 
         public MainWindow()
         {
             InitializeComponent();
-            //this.components = new System.ComponentModel.Container();
-            this.contextMenu1 = new System.Windows.Forms.ContextMenu();
-            this.menuItem1 = new System.Windows.Forms.MenuItem();
-
-            // Initialize contextMenu1
-            this.contextMenu1.MenuItems.AddRange(
-                        new System.Windows.Forms.MenuItem[] { this.menuItem1 });
-
-            // Initialize menuItem1
-            this.menuItem1.Index = 0;
-            this.menuItem1.Text = "E&xit";
-            //this.menuItem1.Click += new System.EventHandler(this.menuItem1_Click);
-
-            // Set up how the form should be displayed.
-           // this.ClientSize = new System.Drawing.Size(292, 266);
-            //this.Text = "Notify Icon Example";
-
-            // Create the NotifyIcon.
-
-            this.notifyIcon = new NotifyIcon();
-            this.notifyIcon.BalloonTipText = "Hello, NotifyIcon!";
-            this.notifyIcon.Text = "I'm the Dragon Warrior!";
-            notifyIcon.Icon = new Icon("res/panda.ico");
-
-            // The ContextMenu property sets the menu that will
-            // appear when the systray icon is right clicked.
-            notifyIcon.ContextMenu = this.contextMenu1;
-
-            // The Text property sets the text that will be displayed,
-            // in a tooltip, when the mouse hovers over the systray icon.
-            notifyIcon.Visible = true;
-
-            //this.notifyIcon.Icon = new System.Drawing.Icon("NotifyIcon.ico");
-            this.notifyIcon.Visible = true;
-            //notifyIcon.MouseDoubleClick += OnNotifyIconDoubleClick;
-            this.notifyIcon.ShowBalloonTip(1000);      
+            buildNotifyIcon();           
         }
+
+        
 
         private void onWindowClosed(object sender, EventArgs e)
         {
             this.notifyIcon.Dispose();
+        }
+
+        private void buildNotifyIcon()
+        {
+            buildNotifyIconMenu();
+
+            this.notifyIcon = new NotifyIcon();
+            this.notifyIcon.BalloonTipText = "Hello, NotifyIcon!";
+            this.notifyIcon.Text = "I'm the Dragon Warrior!";
+            this.notifyIcon.Icon = new Icon("res/panda.ico");
+
+            this.notifyIcon.ContextMenu = this.notifyMenu;
+            
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseClick += OnNotifyIconClick;
+            this.notifyIcon.ShowBalloonTip(1000);      
+        }
+
+
+        bool isHidden = false;
+        
+        private void OnNotifyIconClick(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+
+            if (this.isHidden)
+            {
+                this.Show();
+            }
+            else
+            {
+                this.Hide();
+            }
+            this.isHidden = !this.isHidden;
+        }
+
+        
+        
+        private void buildNotifyIconMenu()
+        {
+            this.notifyMenu = new System.Windows.Forms.ContextMenu();
+            this.notifyMenuItems = new System.Windows.Forms.MenuItem[2];
+            
+            // Initialize menuItem1
+            System.Windows.Forms.MenuItem exitItem = new System.Windows.Forms.MenuItem();
+            this.notifyMenuItems[0] = exitItem;
+            exitItem.Index = 0;
+            exitItem.Text = "E&xit";
+            exitItem.Click += new System.EventHandler(this.exit);
+
+            System.Windows.Forms.MenuItem editItem = new System.Windows.Forms.MenuItem();
+            this.notifyMenuItems[1] = editItem;
+            editItem.Index = 1;
+            editItem.Text = "E&dit";
+            //editItem.Click += new System.EventHandler(this.exit);
+
+            this.notifyMenu.MenuItems.AddRange(this.notifyMenuItems);
+        }
+
+        private void exit(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void onSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            Console.WriteLine(this.WindowState);
         }
     }
 }
